0700927632f3047a385be7e2214b0b2634b190ea
AlexSmith <AlexSmithai@outlook.com>
2025-04-30 15:31:00 -0400

    Delete src directory

--- src/agents/reader_agent.jl
+++
@@ -1,3 +0,0 @@
-function create_reader_agent(id::Int)
-    AIAgent(id, "reader", random_voting_model)
-end
--- src/agents/signer_agent.jl
+++
@@ -1,3 +0,0 @@
-function create_signer_agent(id::Int)
-    AIAgent(id, "signer", random_voting_model)
-end
